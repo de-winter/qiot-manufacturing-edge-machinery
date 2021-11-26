f8b2bf1e7025c7305a478a71d826494e2b71d6fe
Dennis Winter <dennis.winter@viada.de>
2021-11-26 17:46:26 +0100

    change podman parameter

--- run_podman.sh
+++ run_podman.sh
@@ -1,10 +1,10 @@
 podman run -it --rm \
 -v machinery-service-volume:/var/data/qiot/machinery:Z \
--v /home/edge:/bootstrap:Z \
+-v /home/edge/certs:/bootstrap:Z \
 -p 8080:8080/tcp \
 -e  LOG_LEVEL=DEBUG \
--e  MACHINERY_SERIAL=machinery01 \
--e  MACHINERY_NAME=machinery01 \
+-e  MACHINERY_SERIAL=viada_machinery01 \
+-e  MACHINERY_NAME=viada_machinery01 \
 -e  FACILITY_MANAGER_SERVICE_URL=https://facility-manager-service-factory.apps.hackfest.vop.viada.de \
 -e  TS_PATH=/bootstrap/truststore.p12 \
 -e  BROKER_SSL=TRUE \
